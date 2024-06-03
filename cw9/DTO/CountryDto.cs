0fd573f25d608724114736abd960ef302b851b51
Anna Iwanicka <s27389@pjwstk.edu.pl>
2024-06-03 18:22:08 +0200

    Add .gitignore

---
+++ cw9/DTO/CountryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace cw9.DTO;
+
+public class CountryDto
+{
+    public string Name { get; set; }
+}
---
+++ cw9/TripsController.cs
@@ -0,0 +1,31 @@
+﻿using cw9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cw9;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly Cw9Context _context;
+    public TripsController(Cw9Context context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.Select(e => new
+        {
+            Name = e.Name,
+            Countries = e.IdCountries.Select(c => new
+            {
+                Name = c.Name
+            })
+        }).ToListAsync();
+        
+        return Ok(trips);
+    }
+}
